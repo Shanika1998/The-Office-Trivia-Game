ce3358bdedec8f41fac13afc9642c3c4482e2fc4
Nika <sscott004@citymail.cuny.edu>
2023-11-09 20:22:19 -0500

    edit code

--- trivia.js
+++ trivia.js
@@ -1,129 +1,76 @@
-//intialization 
-const submitBtn = document.getElementById('submit');
-const nextButton = document.getElementById('next-button');
-const questionBox = document.getElementById('question-box');
-const questionElement = document.getElementById('question');
-const trueBtn = document.getElementById('True')
-const falseBtn = document.getElementById('False')
-const answerButtonsElement = document.getElementById('answer-buttons')
-const playerScore = document.getElementById('player-score');
-const totalScore = document.getElementById('total-score');
-// Starting score 
-let currentQuestion = 0;
-let score =0;
-
-//Questions for the trivia
 const questions = [
     {
         question:"Who dates Pam's mother?",
-        answers:[
-            {option:"Michael", correct:true},
-            {option:"Dwight", correct:false},
-            
-        ]
+        options:['Michael','Creed', 'Pam', 'Stanley' ],
+        correctAnswer: 0
     },
     {
         question:"What is Michael’s favorite restaurant?",
-        answers:[
-            {option:"Olive Garden", correct:true},
-            {option:"Chili's", correct:false},
+        options:['Olive Garden','Red Lobster', 'Chilis', 'Five Guys'],
+        correctAnswer: 2
         
-        ]
+        
     },
     {
         question:"Where does Jim propose to Pam?",
-        answers:[
-            {option:"The office", correct:true},
-            {option:"The parking lot", correct:false},
-            
-
-        ]
+        options:['The office','Parking Lot', 'Home', 'Holiday Party'],
+        correctAnswer: 1
     },
 
 ]
-//Adding an event to buttons
-nextButton.addEventListener('click', next)
-submitBtn.addEventListener('click',submit)
-//Beginning the Quiz, inputting questions and answer choices into the question element and answer buttons
-function beginQuiz(){
-    currentQuestion =0
-    totalScore.innerHTML = questions.length;
-    questionElement.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML=questions[currentQuestion].answers[0].option;
-    trueBtn.onclick = () => {
-        if(questions[currentQuestion].answers[0].answer){
-            if(score<3){
-                score++
-            }
-        }
-        playerScore.innerText = score++;
-        if(currentQuestion<2){
-            next()
-        }
+
+let currentQuestion = 0
+let score = 0
+
+function loadQuestion() {
+    const questionElement = document.getElementById('question')
+    const optionsElement = document.getElementById('options')
+    if (currentQuestion === questions.length) {
+        showResults()
+        submitButton.style.display = 'none'
+        startOverButton.style.display = 'block'
+        return
+      }
+    
+    const triviaItem = questions[currentQuestion]
+    questionElement.textContent = triviaItem.question
+    optionsElement.innerHTML = ''
+  
+    triviaItem.options.forEach((option, index) => {
+      const optionButton = document.createElement('button');
+      optionButton.textContent = option
+      optionButton.onclick = function() {
+        checkAnswer(index)
+      }
+      optionsElement.appendChild(optionButton);
+    })
+  }
+
+  function checkAnswer(selected) {
+    const triviaItem = questions[currentQuestion];
+  
+    if (selected === triviaItem.correctAnswer) {
+      score++;
+      document.getElementById('result').textContent = 'Correct!'
+    } else {
+      document.getElementById('result').textContent = 'Incorrect!'
     }
-    falseBtn.innerHTML=questions[currentQuestion].answers[1].option
-    falseBtn.onclick = () => {
-        if(questions[currentQuestion].answers[1].answer){
-            if(score<3){
-                score++
-            }
-        }
-        playerScore.innerText= score
-        if(currentQuestion<2){
-            next()
-        }
-    }
+    
+    currentQuestion++
+    loadQuestion()
 }
-beginQuiz()
+    function startOver() {
+        currentQuestion = 0
+        score = 0;
+        document.getElementById('result').textContent = ''
+        loadQuestion()
+      }
+   
 
-function next() {
-    currentQuestion++;
-    if(currentQuestion >= 3) {
-        nextButton.classList.add("hide");
-    }
-    questionElement.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-    trueBtn.onclick = () => {
+  function showResults() {
+    const questionElement = document.getElementById('question')
+    questionElement.textContent = `Quiz Complete! You scored ${score} out of ${questions.length}.`
+  }
 
-        if(questions[currentQuestion].answers[0].answer) {
-
-            if(score < 3) {
-
-                score++;
-
-            }
-
-        }
-
-        playerScore.innerText = score++;
-        if(currentQuestion < 2) {
-            next();
-        }
-    }
- 
-    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-    falseBtn.onclick = () => {
-        if(questions[currentQuestion].answers[1].answer) {
-            if(score < 3) {
-                score;
-            }
- 
-        }
-        playerScore.innerText = score;
-        if(currentQuestion < 2) {
-            next();
-        }
-    }
-} 
-      function submit() {
-        nextButton.classList.add("hide");
-        submitBtn.classList.add("hide");
-        trueBtn.classList.add("hide");
-        falseBtn.classList.add("hide"); 
-        if(playerScore === totalScore){
-            questionElement.innerHTML ="Congratulations on submitting the Quiz! 100%!"
-        }else {
-            questionElement.innerHTML = "Better luck next"
-        }
-     }
-
+  // Start the trivia game
+  loadQuestion();
